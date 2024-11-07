13c0fea1326979e80cfe1e97a109db57383f10f8
JuliaRajn <rajnjulia@gmail.com>
2024-11-07 16:43:12 +0300

    Update lesson0.py

--- lesson0.py
+++ lesson0.py
@@ -1,9 +1,9 @@
-print("1st program")
+#1st program
 print((9**0.5)*5)
-print("2nd program")
+#2nd program
 print(9.99>9.98 and 1000!=1000.1)
-print("3rd program")
+#3rd program
 print(((1234//10)%100)+(5678//10)%100)
-print("4th program")
+#4th program
 print((int(13.42) == int(42.13 % 1 * 100)) or (int(13.42 % 1 *100) == int(42.13)))
 
